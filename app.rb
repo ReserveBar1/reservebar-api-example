79219a2fd37cf8ed950d5ea590436ca6e9a7b885
Jason Knebel <jasonkneb@gmail.com>
2015-04-17 15:44:22 -0400

    fetching brands from local and staging

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ require 'httparty'
 #--------------------App_Routes--------------------#
 
 get '/' do
-  url = "http://localhost:3000/api/products.json"
-  @resp = HTTParty.get(url)
+
+  # staging
+  base_url = "http://staging.reservebar.com/api/"
+  auth = { username: 'admin@reservebar.com', password: 'Reservebar12' }
+
+  # local
+  # base_url = "http://localhost:3000/api/"
+  # auth = {}
+
+  @brands_resp = HTTParty.get(base_url + 'brands.json', basic_auth: auth)
   slim :index
 end
