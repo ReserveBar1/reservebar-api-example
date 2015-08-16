8d969b52d11fc5cf691d882af7e923720e53087f
jfields <justin@railsdog.com>
2015-08-16 11:18:22 -0400

    Create and display order.  Product selection form.

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ gem 'httparty'
 
 group :development do
   gem 'pry'
+  gem 'pry-byebug'
 end
--- app.rb
+++ app.rb
@@ -10,14 +10,24 @@ require 'httparty'
 
 # local
 base_url = 'http://localhost:3000/api/'
-auth = {}
+auth = { username: 'admin@reservebar.com', password: 'Reservebar12' }
 # --------------------App_Routes--------------------#
 
 get '/' do
   @brands_resp = HTTParty.get(base_url + 'brands.json', basic_auth: auth)
+  @brands = @brands_resp['brands'] || ['error']
   @products = HTTParty.get("#{base_url}products.json?brand=Jameson",
                            basic_auth: auth)
   haml :index
+end
+
+get '/brand_products/:brand' do
+  @products = HTTParty.get("#{base_url}products.json?brand=#{params[:brand]}",
+                           basic_auth: auth)
+  puts
+  puts @products.inspect
+  puts
+  haml :products, layout: false
 end
 
 get '/products/:product' do
@@ -27,6 +37,19 @@ get '/products/:product' do
   haml :product
 end
 
+post '/order' do
+  puts "\n\nPARAMS: #{params}\n"
+  body = { line_items: { '0' => { variant_id: params[:product_id],
+                                   quantity: params[:quantity] } } }
+  response = HTTParty.post("#{base_url}orders",
+                           body: body,
+                           basic_auth: auth)
+  @order_status = response.body
+  haml :order
+end
+
 post '/checkout' do
   haml :checkout
 end
+
+
